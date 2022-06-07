0087c11fe8dd97aadd87afd37e74cafe8e50064a
Morgan-Rvl <106060118+Morgan-Rvl@users.noreply.github.com>
2022-06-07 22:48:12 +0200

    Creation timer php
    Le timer fonctionne indépendamment pour chaque appareil mais pour le moment qu'avec des valeurs fixes dans la base .

---
+++ php/response.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    $from_time=date('Y-m-d H:i:s');
+
+    $to_time_Machine1=$_SESSION["end_time_Machine1"];
+    $to_time_Machine2=$_SESSION["end_time_Machine2"];
+    $to_time_Seche_linge1=$_SESSION["end_time_Seche_linge1"];
+    $to_time_Seche_linge2=$_SESSION["end_time_Seche_linge2"];
+
+    $timefirst=strtotime($from_time);
+
+    $timesecond_Machine1=strtotime($to_time_Machine1);
+    $timesecond_Machine2=strtotime($to_time_Machine2);
+    $timesecond_Seche_linge1=strtotime($to_time_Seche_linge1);
+    $timesecond_Seche_linge2=strtotime($to_time_Seche_linge2);
+
+    $differenceinseconds_Machine1=$timesecond_Machine1-$timefirst;
+    $differenceinseconds_Machine2=$timesecond_Machine2-$timefirst;
+    $differenceinseconds_Seche_linge1=$timesecond_Seche_linge1-$timefirst;
+    $differenceinseconds_Seche_linge2=$timesecond_Seche_linge2-$timefirst;
+
+
+    echo gmdate("H:i:s",$differenceinseconds_Machine1),",", gmdate("H:i:s",$differenceinseconds_Machine2),",", gmdate("H:i:s",$differenceinseconds_Seche_linge1),",", gmdate("H:i:s",$differenceinseconds_Seche_linge2);
+?>
