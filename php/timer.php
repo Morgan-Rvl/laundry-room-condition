dd35db3fc29372e4b5997a0e89136ededab1b939
Morgan-Rvl <106060118+Morgan-Rvl@users.noreply.github.com>
2022-07-09 20:28:18 +0200

    Update timer.php

--- php/timer.php
+++ php/timer.php
@@ -43,24 +43,51 @@
         $duration_Seche_linge2=$row["duration"];
     }
 
+    
+
     $_SESSION["duration_Machine1"]=$duration_Machine1;
     $_SESSION["duration_Machine2"]=$duration_Machine2;
     $_SESSION["duration_Seche_linge1"]=$duration_Seche_linge1;
     $_SESSION["duration_Seche_linge2"]=$duration_Seche_linge2;
+    
+    $start_Machine1=mysqli_query($link,"select time_set from timer where id='Machine1'");
+    $start_Machine2=mysqli_query($link,"select time_set from timer where id='Machine2'");
+    $start_Seche_linge1=mysqli_query($link,"select time_set from timer where id='Seche-linge1'");
+    $start_Seche_linge2=mysqli_query($link,"select time_set from timer where id='Seche-linge2'");
 
-    $_SESSION["start_time"]=date("Y-m-d H:i:s");
+    $start_value_Machine1="";
+    $start_value_Machine2="";
+    $start_value_Seche_linge1="";
+    $start_value_Seche_linge2="";
 
-    $end_time_Machine1=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Machine1"].'minutes', strtotime($_SESSION["start_time"])));
-    $end_time_Machine2=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Machine2"].'minutes', strtotime($_SESSION["start_time"])));
-    $end_time_Seche_linge1=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Seche_linge1"].'minutes', strtotime($_SESSION["start_time"])));
-    $end_time_Seche_linge2=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Seche_linge2"].'minutes', strtotime($_SESSION["start_time"])));
+    while($row=mysqli_fetch_array($start_Machine1))
+    {
+        $start_value_Machine1=$row["time_set"];
+    }
+    while($row=mysqli_fetch_array($res_Machine2))
+    {
+        $start_value_Machine2=$row["time_set"];
+    }
+    while($row=mysqli_fetch_array($res_Seche_linge1))
+    {
+        $start_value_Seche_linge1=$row["time_set"];
+    }
+    while($row=mysqli_fetch_array($res_Seche_linge2))
+    {
+        $start_value_Seche_linge2=$row["time_set"];
+    }
+    
+    $end_time_Machine1=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Machine1"].'minutes', strtotime($start_value_Machine1)));
+    $end_time_Machine2=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Machine2"].'minutes', strtotime($start_value_Machine2)));
+    $end_time_Seche_linge1=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Seche_linge1"].'minutes', strtotime($start_value_Seche_linge1)));
+    $end_time_Seche_linge2=date('Y-m-d H:i:s', strtotime('+'.$_SESSION["duration_Seche_linge2"].'minutes', strtotime($start_value_Seche_linge2)));
 
     $_SESSION["end_time_Machine1"]=$end_time_Machine1;
     $_SESSION["end_time_Machine2"]=$end_time_Machine2;
     $_SESSION["end_time_Seche_linge1"]=$end_time_Seche_linge1;
     $_SESSION["end_time_Seche_linge2"]=$end_time_Seche_linge2;
 
-
+    
 ?>
 
 <script type="text/javascript">
